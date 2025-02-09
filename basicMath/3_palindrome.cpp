58fa96b4fb60669f60d0792d4f345b985a5701b9
Abhishek Anand <acedmicabhishek@gmail.com>
2025-02-09 12:17:01 +0530

    well

---
+++ basicMath/3_palindrome.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool palindrome(int n)
+{
+    int revNum = 0;
+    int dup = n;
+
+    while (n > 0) {
+        int ld = n % 10;
+        revNum = (revNum * 10) + ld;
+
+        n = n / 10;
+    }
+
+    if (dup == revNum) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+int main() {
+    int number = 4554;
+
+    if (palindrome(number)) {
+        cout << number << " is a palindrome." << endl;
+    } else {
+        cout << number << " is not a palindrome." << endl;
+    }
+
+    return 0;
+}
